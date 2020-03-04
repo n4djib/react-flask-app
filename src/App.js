c4dd7f1b58968f5d0208b83c96871eeb5a1277c6
n4djib <n4djib@gmail.com>
2020-03-04 11:25:18 +0100

    'finished'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [name, setName] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+      setName(data.name);
+    });
+  }, []); 
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Run it by "yarn dev" (two terminals in one)</p>
+        <p>The current time is: {currentTime}</p>
+        <p>The name is: {name}</p>
+
       </header>
     </div>
   );
